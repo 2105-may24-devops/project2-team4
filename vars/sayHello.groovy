1a5a4090470d179a1d358c5598abb32d39d34206
aypas <tecimler96@gmail.com>
2021-07-06 20:39:35 -0400

    testing-lib

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,13 @@
 #!/usr/bin/env groovy
 
+
+// create 2 scripts. 
+// One to encapsulate the discord bot
+// Another to make sure every neccesary is inside the agent 
+// before running the pipeline...
+
+// might as well add the two top functions into scripts as well.
+
 def hello(String name = 'human') {
     sh "ls"
     return 10
