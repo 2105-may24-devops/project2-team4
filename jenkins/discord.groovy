7e0dbb66522f30b77f6a3ca16e937f2ffc087c90
aypas <tecimler96@gmail.com>
2021-07-07 16:36:25 -0400

    jenkins

--- jenkins/discord.groovy
+++ jenkins/discord.groovy
@@ -2,7 +2,7 @@
 
 def createDescription(dockerChanges, serviceChanges) {
     // both dockerChanges and serviceChanges must have the same keys 
-    def description = "Node Name: $NODE_NAME \n Job Name: $JOB_NAME \n Build Number: $BUILD_NUMBER \n Build ID: $BUILD_ID \n"
+    def description = "Node Name: $NODE_NAME \nJob Name: $JOB_NAME \nBuild Number: $BUILD_NUMBER\n Build ID: $BUILD_ID \n"
 
     for (key in dockerChanges.keySet()) {
         description = "${description}${key}: ${ dockerChanges[key][1] || serviceChanges[key] ? 'updated' : 'unchanged' } \n"
@@ -11,8 +11,8 @@ def createDescription(dockerChanges, serviceChanges) {
 }
 
 
-def sendDiscordMessage(description) {
-    discordSend description: "$description", 
+def sendDiscordMessage(msg) {
+    discordSend description: "$msg", 
         footer: 'something here.', 
         image: '', 
         link: "http://52.142.60.104:8080/jenkins/job/project2-team4/job/$BRANCH_NAME/", 
